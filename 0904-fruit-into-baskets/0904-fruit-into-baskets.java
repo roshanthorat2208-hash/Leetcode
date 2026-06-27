837215c1dcefe044fd58fb39b84d9fa582a21560
roshanthorat2208-hash <roshanthorat2208@gmail.com>
2026-06-27 16:38:45 +0530

    Time: 54 ms (65.99%), Space: 70.3 MB (67.64%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int max =0 ;
+        int i=0,j=0;
+        while(j<fruits.length){
+            map.put(fruits[j],map.getOrDefault(fruits[j],0)+1);
+
+        
+        while(map.size()>2){
+            map.put(fruits[i],map.get(fruits[i])-1);
+        
+        if(map.get(fruits[i])==0){
+            map.remove(fruits[i]);
+        }
+        i++;
+        } 
+        max = Math.max(max,j-i+1);
+        j++;
+    }
+    return max;
+}
+}
