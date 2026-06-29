ccdd4397e8b1f7a05bcc55cc611aef32790d0458
roshanthorat2208-hash <roshanthorat2208@gmail.com>
2026-06-29 19:16:09 +0530

    Time: 3 ms (85.62%), Space: 72 MB (7.29%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -1,18 +1,22 @@
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
-        int maxT = k * threshold;
+        int sum = 0;
         int count = 0;
 
-        for(int i = 0;i<=arr.length-k;i++){
-            int sum = 0;
-            for(int j=i;j<i+k;j++){
-                sum += arr[j];
-            }
-            if(sum>=maxT){
-                count ++;
-            }
+        for(int i=0;i<k;i++){
+            sum += arr[i];
         }
-        return count ;
-        
-    }
+        if(sum>=k*threshold){
+            count++;
+        }
+        for(int i=k;i<arr.length;i++){
+            sum += arr[i];
+            sum -= arr[i-k];
+            if(sum>=k*threshold){
+            count++;
+        }
+            
+        }
+        return count;
+}
 }
